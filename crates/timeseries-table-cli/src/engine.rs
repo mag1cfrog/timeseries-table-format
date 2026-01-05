e7ff08823d817697c44ad4914dbe6b09d80bdf65
mag1cfrog <harrywong2017@gmail.com>
2026-01-04 19:24:46 -0700

    feat: refactor Engine trait and implement DataFusionEngine for async query execution

--- crates/timeseries-table-cli/src/engine.rs
+++ crates/timeseries-table-cli/src/engine.rs
@@ -1,13 +1,42 @@
-use std::future::Future;
+use std::path::{Path, PathBuf};
+
+use crate::{
+    error::{CliError, CliResult},
+    query::{self, QueryOpts, QueryResult},
+};
 
 #[allow(dead_code)]
+#[async_trait::async_trait]
 /// Minimal engine trait.
 pub trait Engine: Send + Sync + 'static {
     type Error: std::error::Error + Send + Sync + 'static;
 
-    type ExecuteFut<'a>: Future<Output = Result<(), Self::Error>> + Send + 'a
-    where
-        Self: 'a;
+    async fn execute(&self, sql: &str, opts: &QueryOpts) -> Result<QueryResult, Self::Error>;
+}
 
-    fn execute<'a>(&'a self, sql: &'a str) -> Self::ExecuteFut<'a>;
+pub struct DataFusionEngine {
+    table_root: PathBuf,
+}
+
+impl DataFusionEngine {
+    pub fn new(table_root: impl AsRef<Path>) -> Self {
+        Self {
+            table_root: table_root.as_ref().to_path_buf(),
+        }
+    }
+
+    pub async fn prepare_session(&self) -> CliResult<query::QuerySession> {
+        query::prepare_session(&self.table_root).await
+    }
+}
+
+#[async_trait::async_trait]
+impl Engine for DataFusionEngine {
+    type Error = CliError;
+
+    async fn execute(&self, sql: &str, opts: &QueryOpts) -> Result<QueryResult, Self::Error> {
+        let session = self.prepare_session().await?;
+        let res = query::run_query(&session, sql, opts).await?;
+        Ok(res)
+    }
 }
