5f4ee659b6874eefed659981506787572e8ff4b8
mag1cfrog <harrywong2017@gmail.com>
2026-02-15 18:03:07 -0700

    feat: add auto-enable functionality for notebook display in Jupyter

--- python/src/timeseries_table_format/__init__.py
+++ python/src/timeseries_table_format/__init__.py
@@ -1,3 +1,8 @@
+from timeseries_table_format.notebook_display import _auto_enable_notebook_display
+from timeseries_table_format.notebook_display import (
+    disable_notebook_display,
+    enable_notebook_display,
+)
 from . import _native as _native
 
 __doc__ = _native.__doc__
@@ -14,6 +19,9 @@ DataFusionError = _native.DataFusionError
 Session = _native.Session
 TimeSeriesTable = _native.TimeSeriesTable
 
+_auto_enable_notebook_display()
+del _auto_enable_notebook_display
+
 __all__ = [
     "__version__",
     "TimeseriesTableError",
@@ -24,4 +32,6 @@ __all__ = [
     "DataFusionError",
     "Session",
     "TimeSeriesTable",
+    "enable_notebook_display",
+    "disable_notebook_display",
 ]
